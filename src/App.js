4cd70433677341c53030267bb81c8d7263006ea9
Rambod Goshtasbi <rpg225@gmail.com>
2024-04-26 01:57:57 -0400

    Setup the React Router Dom Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import SinglePost from "./components/SinglePost.js";
+import Post from "./components/Post.js";
+import Project from "./components/Project.js";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home />} exact/>
+        <Route path="/about" element={<About />} />
+        <Route path='/post/:slug' element={<SinglePost />} />
+        <Route path='/post'  element={<Post/>} />
+        <Route  path='/project' element={<Project />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
