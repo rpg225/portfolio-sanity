12ac9098b73065acd1725f83f732d25f61d33cc4
Rambod Goshtasbi <rpg225@gmail.com>
2024-04-30 01:26:45 -0400

    Added more styling to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer> 
-        I am footer!
+    <footer className='bg-zinc-600 text-center dark:bg-neutral-700 lg:text-left sticky-bottom-0'> 
+       <div className='bg-white/5 p-4 text-center text-surface text-white'> I am footer! </div>
     </footer>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaGithub,FaHtml5, FaCss3, FaReact, FaAccessibleIcon } from "react-icons/fa";
+import { FaGithub,FaHtml5, FaCss3, FaReact, FaAccessibleIcon, FaJsSquare, FaNode } from "react-icons/fa";
+import { SiTypescript, SiSanity } from "react-icons/si";
 
 
 function Home() {
@@ -8,7 +9,7 @@ function Home() {
       <section className="skills bg-gray-100 " id="skills">
       <div className="wrapper py-16">
           <div className="subHeadingContainer">
-            <h1 className='text-3xl md:text-4xl font-bold items-center'>Skills</h1>
+            <h1 className='text-3xl md:text-4xl font-bold text-center'>Skills</h1>
           
           </div>
           <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8'>
@@ -25,16 +26,27 @@ function Home() {
                 <p className='mt-2'>CSS3</p>
               </li>
               <li className='flex flex-col items-center'>
-                <FaHtml5 className='text-4xl text-gray-500'></FaHtml5>
+                <FaJsSquare className='text-4xl text-gray-500'></FaJsSquare>
                 <p className='mt-2'>JavaScript</p>
               </li>
               <li className='flex flex-col items-center'>
-                <FaReact className='text-4xl text-gray-500'></FaReact>
-                <p className='mt-2'>React</p>
+                <SiTypescript className='text-4xl text-gray-500'></SiTypescript>
+                <p className='mt-2'>TypeScript</p>
               </li>
               <li className='flex flex-col items-center'>
                 <FaGithub className='text-4xl text-gray-500'></FaGithub>
                 <p className='mt-2'>Github</p>
+              </li>
+              <li className='flex flex-col items-center'>
+                <FaReact className='text-4xl text-gray-500'></FaReact>
+                <p className='mt-2'>React</p>
+              </li><li className='flex flex-col items-center'>
+                <SiSanity className='text-4xl text-gray-500'></SiSanity>
+                <p className='mt-2'>Sanity</p>
+              </li>
+              <li className='flex flex-col items-center'>
+                <FaNode className='text-4xl text-gray-500'></FaNode>
+                <p className='mt-2'>Node</p>
               </li>
 
           </ul>
